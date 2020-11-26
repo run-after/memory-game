24670b8599656723019e3ee1c0ed221d8e6e18f0
run-after <run.after@protonmail.com>
2020-11-26 09:58:29 -0600

    Style main app page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import './App.css';
+import './styles/App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [hiScore, setHiScore] = useState(0);
+
+  const incrementScore = () => {
+    setScore(score + 1);
+  };
+
+  const newHiScore = () => {
+    setHiScore(score);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Memory Game</h1>
+        <div className='score-card'>
+          <h6>Score: {score} </h6>
+          <h6>Hi Score: {hiScore} </h6>
+        </div>
       </header>
+      <main>
+        <div className='intro'>
+          Choose each card once. Do not select the same card twice, otherwise you lose.
+        </div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+        <div className='card'></div>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// Don't use bootstrap. Roll your own
